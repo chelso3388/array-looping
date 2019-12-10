c6c147d00e5616cb1adcce46aa1407e97cec0c47
Student <example@example.com>
2019-12-09 20:48:05 -0800

    add cleaning up looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-var food = $("input#food").val();
-var season = $("input#food").val();
-var animal = $("input#food").val();
-$(".food").text(foodInput);
-$(".season").text(seasonInput);
-$(".animal").text(animalInput);
-
-$("#answers").show();
-
- even.preventDefault();
-});
-});
+var fruits = ["apple", "orange", "passion fruit", "kiwi"];
+var seasons =["fall", "winter", "spring", "summer"];
+var animals =["sloth", "cat", "dog"];
